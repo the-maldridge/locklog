99df681d9b6e6cc20cf6bdd8327f26f1a004ceec
Michael Aldridge <aldridge.mac@gmail.com>
2014-10-31 15:01:20 -0500

    split functions and config out

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$SERVER="corespace.michaelwashere.tk";
+$PATH="/lockout/";
+$DBUSER="deskworker";
+$DBPASS="foobar";
+$DBHOST="localhost";
+$DBNAME="locklog";
+$LOGTABLE="lockouts";
+$HISTTABLE="history";
+$EMAILTHRESHOLD=4;
+$EMAILFROM="LockLog";
+$EMAILSUBJECT="Automated lockout report";
+?>
