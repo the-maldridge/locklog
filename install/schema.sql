6551606b8aeb97e641d3b0d4d6f7fe1b13db9c1b
Michael Aldridge <aldridge.mac@gmail.com>
2014-10-31 15:00:48 -0500

    add schema for database

---
+++ install/schema.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2+deb7u1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 31, 2014 at 02:38 PM
+-- Server version: 5.5.40
+-- PHP Version: 5.4.4-14+deb7u14
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `locklog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE IF NOT EXISTS `history` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `res_id` int(11) NOT NULL,
+  `total_max` int(11) NOT NULL,
+  `local_max` int(11) NOT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+
+--
+-- Table structure for table `lockouts`
+--
+
+CREATE TABLE IF NOT EXISTS `lockouts` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `PA_name` varchar(35) NOT NULL,
+  `bldg` varchar(11) NOT NULL,
+  `res_name` varchar(35) NOT NULL,
+  `res_id` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=91 ;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
