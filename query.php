6b747283afdec33d09587461101ac1698550df01
Michael Aldridge <aldridge.mac@gmail.com>
2014-11-04 21:03:43 -0600

    all fields displayed correctly

--- query.php
+++ query.php
@@ -7,51 +7,87 @@ $config = getConfig("buildings.json");
 <html>
 <head>
 <title>LockLog Query</title>
+ <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
 </head>
 <body>
-<form method="POST" action="query.php">
 <table>
 <tr><td colspan="2">Filter By:</td></tr>
-<tr>
-<td>Building:</td>
-<td><input type='checkbox' id='bldg' onclick='toggleCollapse("bldg", "bldgHTML")'></td>
-</tr>
+  <tr>
+  <td>Building:</td>
+  <td><input type='checkbox' id='bldg' onclick='toggleCollapse("bldg", "bldgHTML")'></td>
+  </tr>
+  
+  <tr>
+  <td>Name:</td>
+  <td><input type='checkbox' id='name' onclick='toggleCollapse("name", "nameHTML")'></td>
+  </tr>
+  
+  <tr>
+  <td>UTD ID:</td>
+  <td><input type='checkbox' id='id' onclick='toggleCollapse("id", "idHTML")'></td>
+  </tr>
+  
+  <tr>
+  <td>PA:</td>
+  <td><input type='checkbox' id='pa' onclick='toggleCollapse("pa", "paHTML")'></td>
+  </tr>
+  
+  <tr>
+  <td>After:</td>
+  <td><input type='checkbox' id='dateAfter' onclick='toggleCollapse("dateAfter", "dateAfterHTML")'></td>
+  </tr>
+  
+  <tr>
+  <td>Before:</td>
+  <td><input type='checkbox' id='dateBefore' onclick='toggleCollapse("dateBefore", "dateBeforeHTML")'></td>
+  </tr>
+  
+  <table>
+  <form method="GET" action="query.php"> 
+  <tr id='bldgHTML' style='visibility: collapse;'>
+  <?php getBuilding($config, true); ?>
+  </tr>
+  
+  <tr id='nameHTML' style='visibility: collapse;'>
+  <td>Name:</td>
+  <td><input type='text' name='name'></input></td>
+  </tr>
+  
+  <tr id='idHTML' style='visibility: collapse;'>
+  <td>UTD ID:</td>
+  <td><input type='text' name='id'></input></td>
+  </tr>
+  
+  <tr id='paHTML' style='visibility: collapse;'>
+  <td>PA Name:</td>
+  <td><input type='text' name='pa'></input></td>
+  </tr>
+  
+  <tr id='dateAfterHTML' style='visibility: collapse;'>
+  <td>On or After:</td>
+  <td><input type='text' id='datepicker1' name='dateAfter'></input>
+  </td>
+  </tr>
+  
+  <tr id='dateBeforeHTML' style='visibility: collapse;'>
+  <td>On or Before:</td>
+  <td><input type='text' id='datepicker2' name='dateBefore'></input></td>
+  </tr>
+  
+  <tr>
+  <td colspan='2'><input type='submit' name='run' value='Run Query'></td>
+  </tr>
 
-<tr>
-<td>Name:</td>
-<td><input type='checkbox' id='name' onclick='toggleCollapse("name", "nameHTML")'></td>
-</tr>
-
-<tr>
-<td>UTD ID:</td>
-<td><input type='checkbox' id='id' onclick='toggleCollapse("id", "idHTML")'></td>
-</tr>
-
-<tr>
-<td>PA:</td>
-<td><input type='checkbox' id='pa' onclick='toggleCollapse("pa", "paHTML")'></td>
-</tr>
-<table>
-<tr id='bldgHTML' style='visibility: collapse;'>
-<?php getBuilding($config, true); ?>
-</tr>
-
-<tr id='nameHTML' style='visibility: collapse;'>
-<td>Name:</td>
-<td><input type='text' name='name'></input></td>
-</tr>
-
-<tr id='idHTML' style='visibility: collapse;'>
-<td>UTD ID:</td>
-<td><input type='text' name='id'></input></td>
-</tr>
-
-<tr id='paHTML' style='visibility: collapse;'>
-<td>PA Name:</td>
-<td><input type='text' name='pa'></input></td>
-</tr>
-</table>
-</form>
-</body>
-<script src='queryHelpers.js'></script>
+  </form>  
+  </table>
+  </body>
+  <script src='queryHelpers.js'></script>
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+  <script>
+  $(function() {
+      $("#datepicker1").datepicker({dateFormat:"yy-mm-dd"});
+      $("#datepicker2").datepicker({dateFormat:"yy-mm-dd"});
+    });
+</script>
 </html>
