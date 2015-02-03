091e91b09db04d6a4e3b433800209c0cd44463a0
Michael Aldridge <aldridge.mac@gmail.com>
2015-02-03 02:19:25 -0600

    add connect and basic history functions

--- db.py
+++ db.py
@@ -1,6 +1,77 @@
-import sqlite3, MySQLdb
-
+import sqlite3, MySQLdb, logging
 class Database():
     def __init__(self, config):
-        config = config["database"]
-        
+        self.logger = logging.getLogger("DB-DRVR")
+        try:
+            config = config["database"]
+            self.logger.debug(config)
+        except KeyError, e:
+            self.logger.critical("Unable to load database configuration")
+            raise e
+
+        if config["mode"]=="sqlite":
+            self.logger.info("Selecting SQLite database driver")
+            self.conn = sqlite3.connect(config["path"])
+        elif config["mode"]=="mysql":
+            self.logger.info("Selecting mySQL database driver")
+            host = config["host"]
+            user = config["user"]
+            passwd = config["pass"]
+            db = config["db"]
+            try:
+                self.logger.info("Attempting to connect to %s on %s", db, host)
+                self.conn = MySQLdb.connect(host, user, passwd, db)
+                self.logger.info("Connection successful")
+            except:
+                self.logger.error("Serious database connection error occured")
+        else:
+            logging.error("Only SQLite and MySQL are supported at this time")
+
+        try:
+            self.logger.debug("Attempting to aquire DB cursor")
+            self.c = self.conn.cursor()
+            self.logger.debug("Aquired DB cursor")
+        except:
+            self.logger.error("Could not aquire DB cursor")
+            
+
+    def addLockout(self, PA, bldg, res_name, res_id):
+        SQL="INSERT INTO lockouts (PA_name, bldg, res_name, res_id) VALUES (%s, %s, %s, %s)"
+        self.logger.info("Logging lockout for %s by %s in %s", res_name, PA, bldg)
+        self.c.execute(SQL, (PA, bldg, res_name, res_id))
+        self.conn.commit()
+
+    def addToHistory(self, res_id):
+        SQL="INSERT INTO history (res_id, local_max, total_max) VALUES(%s, 1, 1)"
+        self.logger.debug("First lockout for %s", res_id)
+        self.c.execute(SQL, (res_id))
+        self.conn.commit()
+
+    def checkHistory(self, res_id):
+      SQL="SELECT * FROM history WHERE res_id=%s"
+      self.c.execute(SQL, (res_id))
+      exists=False
+      if self.c.fetchall() is not None:
+          exists=True
+      self.logger.debug("Exists? " + str(exists))
+
+    def updateHistory(self, res_id):
+        SQL="SELECT * FROM history WHERE `res_id`=%s"
+        self.logger.debug("Selecting data for id #%s", res_id)
+        if self.c.execute(SQL, (res_id)):
+            data = self.c.fetchone()
+        logging.debug("Data: " + str(data))
+        index = data[0]
+        local = data[2] + 1
+        total = data[3] + 1
+        SQL="UPDATE history SET total_max=%s,local_max=%s WHERE `index`=%s"
+        self.c.execute(SQL, (total, local, index))
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    import json
+    db = Database(json.load(open("config.json")))
+    db.addLockout("michael", "RHW", "foo", "1234")
+    #db.addToHistory(1234)
+    db.checkHistory(1234)
+    db.updateHistory(1234)
